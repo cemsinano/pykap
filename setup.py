b8c35088a47edc9f4eaac517b17347c8965c889b
kkarakaya <i.presentable@gmail.com>
2022-12-05 19:55:07 +0300

    Revert "Update setup.py"
    This reverts commit 8bedae678a8d8c0efd4a0599dbb874e4ec7912d4.

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ install_required = ['bs4','pandas','requests','regex','html5lib']
 
 setuptools.setup(
     name="pykap",
-    version="0.0.0.0",
-    author="presentable",
-    author_email="i.presentable@gmail.com",
+    version="0.0.2.2",
+    author="Cem Sinan Ozturk",
+    author_email="cemsinanozturk@gmail.com",
     description="KAP (Public Disclosure Platform) Documentation Wrapper for Capital Markets Board of Turkey and Borsa Istanbul Public Disclosures",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kkarakaya/pykap",
+    url="https://github.com/cemsinano/pykap",
     install_requires = install_required,
     project_urls={
-        "Bug Tracker": "https://github.com/kkarakaya/pykap/issues",
+        "Bug Tracker": "https://github.com/cemsinano/pykap/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
